dd56bdd67e0ab2a548e086e2b0d305342c4dce17
William Sanches <william.sanches@plataformatec.com.br>
2019-04-30 13:16:17 -0300

    WIP

--- day03-2.exs
+++ day03-2.exs
@@ -7,6 +7,7 @@ defmodule Day_03_2 do
   def process(input) do
     input
     |> parse_lines()
+    |> keep_disjointed()
   end
 
   def parse_lines(lines) do
@@ -16,13 +17,17 @@ defmodule Day_03_2 do
   end
 
   def parse_line(line) do
-    regex = ~r/#\d+ @ (\d+),(\d+): (\d+)x(\d+)/
+    regex = ~r/#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/
 
-    [_, left, top, width, heigth] = Regex.run(regex, line)
+    [_ | data] = Regex.run(regex, line)
 
-    [left, top, width, heigth]
-    |> Enum.map(fn item -> String.to_integer(item) end)
-    |> List.to_tuple()
+    [id, left, top, width, heigth] = Enum.map(data, &String.to_integer/1)
+
+    List.to_tuple([id, left..(left + width - 1), top..(top + heigth - 1)])
+  end
+
+  def keep_disjointed(claims) do
+    claims
   end
 end
 
@@ -46,8 +51,10 @@ defmodule Day_03_2Test do
   test "parse_line/1" do
     input = "#1 @ 1,3: 4x4"
 
-    assert Day_03_2.parse_line(input) == {1, 3, 4, 4}
+    assert Day_03_2.parse_line(input) == {1, 1..4, 3..6}
   end
+
+  test "keep_disjointed/1"
 
   test "parse_lines/1" do
     input = """
@@ -57,9 +64,9 @@ defmodule Day_03_2Test do
     """
 
     assert Day_03_2.parse_lines(input) == [
-             {1, 3, 4, 4},
-             {3, 1, 4, 4},
-             {5, 5, 2, 2}
+             {1, 1..4, 3..6},
+             {2, 3..6, 1..4},
+             {3, 5..6, 5..6}
            ]
   end
 end
